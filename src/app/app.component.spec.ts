af513df56d4a4cbc35a0638c23fec48bc543808f
mohankiran <mohankiran39@gmail.com>
2023-04-11 16:00:55 +0530

    Test case update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('login-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('login-app app is running in env320490 !!');
   });
 });
